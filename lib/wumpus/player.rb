03ffad1d7382fb55209856c6bd844e421007915a
Thanh Nguyen <thanh.trade@gmail.com>
2015-12-28 23:40:36 +0700

    player modelling

---
+++ lib/wumpus/player.rb
@@ -0,0 +1,37 @@
+module Wumpus
+	class Player
+		def initialize
+			@senses = {}
+			@encounters = {}
+			@actions = {}
+		end
+
+		attr_reader :room
+
+		def sense(thing, &callback)
+			@sense[thing] = callback
+		end
+
+		def encounter(thing, &callback)
+			@encounters[thing] = callback
+		end
+
+		def action(thing, &callback)
+			@actions[thing] = callback
+		end
+
+		def enter(room)
+			@room = room
+
+			@encounters.each do |thing, action|
+				return(action.call) if room.has?(thing)
+			end
+		end
+
+		def explore_room
+			@senses.each do |thing, action|
+				action.call if @room.neighbors.any? { |e| e.has?(thing) }
+			end
+		end
+	end
+end
